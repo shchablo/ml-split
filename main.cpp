c0660fd9d46a8908a0dffe38cae8c3f021c05f1c
Shchablo <shchabloKV@gmail.com>
2020-07-20 16:28:54 +0200

    0.0 retab

--- main.cpp
+++ main.cpp
@@ -4,155 +4,155 @@
 #include <ctime> 
 
 std::shared_ptr<ISplitter> SplitterCreate(/*[in]*/ int _nMaxBuffers, /*[in]*/ int _nMaxClients) {
-	std::shared_ptr<ISplitter> splitter(new ISplitter(_nMaxBuffers,  _nMaxClients));
-	return splitter;
+  std::shared_ptr<ISplitter> splitter(new ISplitter(_nMaxBuffers,  _nMaxClients));
+  return splitter;
 }
 
 
 int create_test(const std::shared_ptr<ISplitter>& splitter, int _nMaxBuffers,  int _nMaxClients, std::vector<int>* ids) 
 {
-	// ------
-	int overClients = 4;
+  // ------
+  int overClients = 4;
   int id = 0;
-	std::cout << "Trying to add Clients N=" << _nMaxClients +  overClients << "!" << std::endl;	
-	for(int i = 0; i < _nMaxClients + overClients; i++) {
+  std::cout << "Trying to add Clients N=" << _nMaxClients +  overClients << "!" << std::endl;  
+  for(int i = 0; i < _nMaxClients + overClients; i++) {
     bool isID = splitter->SplitterClientAdd(&id);
-		std::cout << "id:" << id << "|" << (int)isID << " ";
+    std::cout << "id:" << id << "|" << (int)isID << " ";
     ids->push_back(id);
   }
-	std::cout << std::endl;
-	
-	// ------
+  std::cout << std::endl;
+  
+  // ------
   srand (time (NULL)); // + include ctime
-	int deleteID =  rand () % (ids->size() - 1) + 1; 
-	bool isDeleteID = splitter->SplitterClientRemove(ids->at(deleteID));
-	std::cout << "What to delete ID: " << ids->at(deleteID)  << " -> " << "Result: " << isDeleteID << "!" <<  std::endl;	
-	isDeleteID = splitter->SplitterClientRemove(deleteID);
-	std::cout << "What to delete ID: " << deleteID  << " -> " << "Result: " << isDeleteID << "!" << std::endl;	
-	// ------
-	
-	// ------
-	std::cout << "Trying to get id all clients" << std::endl;	
-	int client = -1;
-	int latency = -1;
-	for(int i = 0; i < _nMaxClients; i++) {
-		bool is = splitter->SplitterClientGetByIndex(i, &client, &latency);
-		if(is)
-			std::cout << "id:" << client << "|" << "c:" << latency << " ";
-	}
-	std::cout << std::endl;
+  int deleteID =  rand () % (ids->size() - 1) + 1; 
+  bool isDeleteID = splitter->SplitterClientRemove(ids->at(deleteID));
+  std::cout << "What to delete ID: " << ids->at(deleteID)  << " -> " << "Result: " << isDeleteID << "!" <<  std::endl;  
+  isDeleteID = splitter->SplitterClientRemove(deleteID);
+  std::cout << "What to delete ID: " << deleteID  << " -> " << "Result: " << isDeleteID << "!" << std::endl;  
+  // ------
+  
+  // ------
+  std::cout << "Trying to get id all clients" << std::endl;  
+  int client = -1;
+  int latency = -1;
+  for(int i = 0; i < _nMaxClients; i++) {
+    bool is = splitter->SplitterClientGetByIndex(i, &client, &latency);
+    if(is)
+      std::cout << "id:" << client << "|" << "c:" << latency << " ";
+  }
+  std::cout << std::endl;
 
 return 1;
 }
 
 int fill(const std::shared_ptr<ISplitter>& splitter, int nTimeOutMsec, int times) 
 {
-	uint8_t n = 1; int N = times;
-	while(n < N) {
-	std::shared_ptr<std::vector<uint8_t>> data(new std::vector<uint8_t>());
-		data->push_back(n);
- 		int delay = splitter->SplitterPut(data, nTimeOutMsec); // tmp 
-		std::this_thread::sleep_for(std::chrono::milliseconds(nTimeOutMsec*2 - delay)); n = n + 1;
-		
+  uint8_t n = 1; int N = times;
+  while(n < N) {
+  std::shared_ptr<std::vector<uint8_t>> data(new std::vector<uint8_t>());
+    data->push_back(n);
+     int delay = splitter->SplitterPut(data, nTimeOutMsec); // tmp 
+    std::this_thread::sleep_for(std::chrono::milliseconds(nTimeOutMsec*2 - delay)); n = n + 1;
+    
     //for(unsigned int i = 0; i < data->size(); i++)
-		//	std::cout << (int)data->at(i) << " ";
-		//std::cout << std::endl;
-	}
+    //  std::cout << (int)data->at(i) << " ";
+    //std::cout << std::endl;
+  }
 
-	return 1;	
+  return 1;  
 } 
 
 int get(const std::shared_ptr<ISplitter>& splitter, int nTimeOutMsec, int times, int ID, std::deque<std::shared_ptr<std::vector<uint8_t>>>* buf, int extra) 
 {
-	uint8_t n = 1; int N = times;
-	while(n < N) {
-		std::shared_ptr<std::vector<uint8_t>> data(new std::vector<uint8_t>());
- 		int isGet = splitter->SplitterGet(ID, data, nTimeOutMsec);
+  uint8_t n = 1; int N = times;
+  while(n < N) {
+    std::shared_ptr<std::vector<uint8_t>> data(new std::vector<uint8_t>());
+     int isGet = splitter->SplitterGet(ID, data, nTimeOutMsec);
     if(!isGet) buf->push_back(data);
-		std::this_thread::sleep_for(std::chrono::milliseconds(nTimeOutMsec)); n = n + 1;
-		if(n == 5) std::this_thread::sleep_for(std::chrono::milliseconds(extra));
-		//if(n == 7) splitter->SplitterFlush();
-		//if(n == 7) splitter->SplitterClose();
-		
+    std::this_thread::sleep_for(std::chrono::milliseconds(nTimeOutMsec)); n = n + 1;
+    if(n == 5) std::this_thread::sleep_for(std::chrono::milliseconds(extra));
+    //if(n == 7) splitter->SplitterFlush();
+    //if(n == 7) splitter->SplitterClose();
+    
     //for(unsigned int i = 0; i < data->size(); i++)
-		//	std::cout << (int)data->at(i) << " ";
-		//std::cout << std::endl;
-	}
+    //  std::cout << (int)data->at(i) << " ";
+    //std::cout << std::endl;
+  }
 
-	return 1;	
+  return 1;  
 } 
 int main(void)
 {
-	int _nMaxBuffers = 2; int _nMaxClients = 10;
-	std::shared_ptr<ISplitter> splitter =  SplitterCreate(_nMaxBuffers, _nMaxClients); 
-	std::cout << "Splitter B=" << _nMaxBuffers << " C=" << _nMaxClients << std::endl;	
-	
+  int _nMaxBuffers = 2; int _nMaxClients = 10;
+  std::shared_ptr<ISplitter> splitter =  SplitterCreate(_nMaxBuffers, _nMaxClients); 
+  std::cout << "Splitter B=" << _nMaxBuffers << " C=" << _nMaxClients << std::endl;  
+  
   std::vector<int> ids;
-	int result = create_test(splitter,  _nMaxBuffers,  _nMaxClients, &ids);
-	std::cout << "create_test: " << result << std::endl;
-	
+  int result = create_test(splitter,  _nMaxBuffers,  _nMaxClients, &ids);
+  std::cout << "create_test: " << result << std::endl;
+  
 
   int pulses_read = 30;
   int pulses_fill = 10;
-	std::deque<std::shared_ptr<std::vector<uint8_t>>> cbuf;
-	std::deque<std::shared_ptr<std::vector<uint8_t>>> cbuf2;
-	std::deque<std::shared_ptr<std::vector<uint8_t>>> cbuf3;
- 	std::thread in_1 (fill, splitter, 50, pulses_fill +1); 
- 	//std::thread in_2 (fill, splitter, 50, pulses_fill +1); 
- 	std::thread out_1 (get, splitter, 50, pulses_read +1, ids.at(0), &cbuf, 500); 
- 	std::thread out_2 (get, splitter, 50, pulses_read +1, ids.at(1), &cbuf2, 0); 
- 	std::thread out_3 (get, splitter, 50, pulses_read +1, ids.at(2), &cbuf3, 400); 
+  std::deque<std::shared_ptr<std::vector<uint8_t>>> cbuf;
+  std::deque<std::shared_ptr<std::vector<uint8_t>>> cbuf2;
+  std::deque<std::shared_ptr<std::vector<uint8_t>>> cbuf3;
+   std::thread in_1 (fill, splitter, 50, pulses_fill +1); 
+   //std::thread in_2 (fill, splitter, 50, pulses_fill +1); 
+   std::thread out_1 (get, splitter, 50, pulses_read +1, ids.at(0), &cbuf, 500); 
+   std::thread out_2 (get, splitter, 50, pulses_read +1, ids.at(1), &cbuf2, 0); 
+   std::thread out_3 (get, splitter, 50, pulses_read +1, ids.at(2), &cbuf3, 400); 
   out_1.join();
   out_2.join();
   out_3.join();
   in_1.join();
   //in_2.join();
 
-	std::cout << "Client Buffer3: size=" << cbuf3.size() << std::endl;
+  std::cout << "Client Buffer3: size=" << cbuf3.size() << std::endl;
   for (unsigned i=0; i < cbuf3.size(); i++) {
-  	for (unsigned j=0; j < cbuf3.at(i)->size(); j++)
+    for (unsigned j=0; j < cbuf3.at(i)->size(); j++)
     std::cout << (int)cbuf3.at(i)->at(j) << " ";
-	}
+  }
   std::cout << std::endl;
-	// ------
-	
-	std::cout << "Client Buffer2: size=" << cbuf2.size() << std::endl;
+  // ------
+  
+  std::cout << "Client Buffer2: size=" << cbuf2.size() << std::endl;
   for (unsigned i=0; i < cbuf2.size(); i++) {
-  	for (unsigned j=0; j < cbuf2.at(i)->size(); j++)
+    for (unsigned j=0; j < cbuf2.at(i)->size(); j++)
     std::cout << (int)cbuf2.at(i)->at(j) << " ";
-	}
+  }
   std::cout << std::endl;
-	// ------
+  // ------
 
-	std::cout << "Client Buffer: size=" << cbuf.size() << std::endl;
+  std::cout << "Client Buffer: size=" << cbuf.size() << std::endl;
   for (unsigned i=0; i < cbuf.size(); i++) {
-  	for (unsigned j=0; j < cbuf.at(i)->size(); j++)
+    for (unsigned j=0; j < cbuf.at(i)->size(); j++)
     std::cout << (int)cbuf.at(i)->at(j) << " ";
-	}
+  }
   std::cout << std::endl;
-	// ------
+  // ------
 
-	std::deque<std::shared_ptr<std::vector<uint8_t>>> buf = splitter->getBuffer();
-	std::cout << "Buffer: size=" << buf.size() << std::endl;
+  std::deque<std::shared_ptr<std::vector<uint8_t>>> buf = splitter->getBuffer();
+  std::cout << "Buffer: size=" << buf.size() << std::endl;
   for (unsigned i=0; i < buf.size(); i++) {
-  	for (unsigned j=0; j < buf.at(i)->size(); j++)
+    for (unsigned j=0; j < buf.at(i)->size(); j++)
     std::cout << (int)buf.at(i)->at(j) << " ";
-	}
+  }
   std::cout << std::endl;
-	// ------
-	std::cout << "Clients status" << std::endl;	
-	int client = -1;
-	int latency = -1;
-	for(int i = 0; i < _nMaxClients; i++) {
-		bool is = splitter->SplitterClientGetByIndex(i, &client, &latency);
-		if(is)
-			std::cout << "id:" << client << "|" << "c:" << latency << " ";
-	}
-	std::cout << std::endl;
-	
+  // ------
+  std::cout << "Clients status" << std::endl;  
+  int client = -1;
+  int latency = -1;
+  for(int i = 0; i < _nMaxClients; i++) {
+    bool is = splitter->SplitterClientGetByIndex(i, &client, &latency);
+    if(is)
+      std::cout << "id:" << client << "|" << "c:" << latency << " ";
+  }
+  std::cout << std::endl;
+  
 
-	int out = 0;
-	std::cin >> out; 
-	
-	return 0;
+  int out = 0;
+  std::cin >> out; 
+  
+  return 0;
 }
--- src/ISplitter.cpp
+++ src/ISplitter.cpp
@@ -3,9 +3,9 @@
 
 ISplitter::ISplitter(int _nMaxBuffers, int _nMaxClients)
 {
-		__nMaxBuffers = _nMaxBuffers;
-		__nMaxClients =  _nMaxClients;
-}  	
+    __nMaxBuffers = _nMaxBuffers;
+    __nMaxClients =  _nMaxClients;
+}    
 ISplitter::~ISplitter() 
 {
 }
@@ -19,147 +19,147 @@ bool ISplitter::SplitterInfoGet(int* _pnMaxBuffers, int* _pnMaxClients)
 int ISplitter::SplitterPut(const std::shared_ptr<std::vector<uint8_t>>& _pVecPut, int _nTimeOutMsec)
 {
   auto t1 = std::chrono::high_resolution_clock::now();
-	int result = 0;
-	long ID = static_cast<long>(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
-	__mtxQS.lock(); 
-	__qSupervisor.push(ID);  
-	__supervisor	=  __qSupervisor.front(); 
-	__isSupervisor	=  true; 
-	__mtxQS.unlock();
-	
-	bool isWrite = false;
+  int result = 0;
+  long ID = static_cast<long>(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+  __mtxQS.lock(); 
+  __qSupervisor.push(ID);  
+  __supervisor  =  __qSupervisor.front(); 
+  __isSupervisor  =  true; 
+  __mtxQS.unlock();
+  
+  bool isWrite = false;
   std::vector<int> outsiders;
-	int maxCount = 0; 
-	__mtx.lock();
-	for(auto& it: __clients) {
-		if(maxCount < it.second) maxCount =  it.second;
-		if(__nMaxBuffers == it.second)
-			outsiders.push_back(it.first);
-	}
-	if(maxCount < __nMaxBuffers && __supervisor == ID) { 
-		if((int)__buffer.size() == __nMaxBuffers && __buffer.size() > 0)
+  int maxCount = 0; 
+  __mtx.lock();
+  for(auto& it: __clients) {
+    if(maxCount < it.second) maxCount =  it.second;
+    if(__nMaxBuffers == it.second)
+      outsiders.push_back(it.first);
+  }
+  if(maxCount < __nMaxBuffers && __supervisor == ID) { 
+    if((int)__buffer.size() == __nMaxBuffers && __buffer.size() > 0)
       __buffer.pop_front();
     __buffer.push_back(_pVecPut);
-		for(auto& it: __clients)
-			it.second = it.second + 1;
-		isWrite = true;
-	} else isWrite = false;
-	__mtx.unlock();
-	
-	if(isWrite) {
-		__mtxQS.lock(); 
-		__qSupervisor.pop();  
-		if( __qSupervisor.size() > 0) {
-			__supervisor	=  __qSupervisor.front(); 
-			__isSupervisor	=  true;
-		}
-		else __isSupervisor  =  false; 
-		__mtxQS.unlock();
-		result = 0;
-	}
+    for(auto& it: __clients)
+      it.second = it.second + 1;
+    isWrite = true;
+  } else isWrite = false;
+  __mtx.unlock();
+  
+  if(isWrite) {
+    __mtxQS.lock(); 
+    __qSupervisor.pop();  
+    if( __qSupervisor.size() > 0) {
+      __supervisor  =  __qSupervisor.front(); 
+      __isSupervisor  =  true;
+    }
+    else __isSupervisor  =  false; 
+    __mtxQS.unlock();
+    result = 0;
+  }
   
   __isSupervisor  =  false; 
-	auto t2 = std::chrono::high_resolution_clock::now();
-	while(!isWrite && std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() < _nTimeOutMsec) {	
+  auto t2 = std::chrono::high_resolution_clock::now();
+  while(!isWrite && std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() < _nTimeOutMsec) {  
     for(unsigned int i = 0;  i < outsiders.size(); i++) {
-			if(__client == outsiders.at(i))
- 				 outsiders.erase(outsiders.begin()+i);
-		}
-		if(outsiders.size() == 0 && __supervisor == ID) {
-  		__isSupervisor  =  true; 
-			__mtx.lock();
-			__buffer.pop_front();
-			__buffer.push_back(_pVecPut);
-			for(auto& it: __clients)
-				it.second = it.second + 1;
-			isWrite = true;
-			__mtx.unlock();
-		}
-		if(isWrite) {
-			__mtxQS.lock(); 
-			__qSupervisor.pop();  
-			if( __qSupervisor.size() > 0) {
-				__supervisor	=  __qSupervisor.front(); 
-				__isSupervisor	=  true;
-			}
-			else __isSupervisor  =  false; 
-			__mtxQS.unlock();
-		result = 0;
-		}	
+      if(__client == outsiders.at(i))
+          outsiders.erase(outsiders.begin()+i);
+    }
+    if(outsiders.size() == 0 && __supervisor == ID) {
+      __isSupervisor  =  true; 
+      __mtx.lock();
+      __buffer.pop_front();
+      __buffer.push_back(_pVecPut);
+      for(auto& it: __clients)
+        it.second = it.second + 1;
+      isWrite = true;
+      __mtx.unlock();
+    }
+    if(isWrite) {
+      __mtxQS.lock(); 
+      __qSupervisor.pop();  
+      if( __qSupervisor.size() > 0) {
+        __supervisor  =  __qSupervisor.front(); 
+        __isSupervisor  =  true;
+      }
+      else __isSupervisor  =  false; 
+      __mtxQS.unlock();
+    result = 0;
+    }  
   t2 = std::chrono::high_resolution_clock::now();
-	}
+  }
  
-	if(!isWrite) {
-		__mtx.lock();
-		__buffer.pop_front();
-		__buffer.push_back(_pVecPut);
-		for(auto& it: __clients) {
-			if(it.second < __nMaxBuffers)
-				it.second = it.second + 1;
-		}
-		__mtx.unlock();
-		
-		__mtxQS.lock(); 
-		__qSupervisor.pop();  
-		if( __qSupervisor.size() > 0) {
-			__supervisor	=  __qSupervisor.front(); 
-			__isSupervisor	=  true;
-		}
-		else __isSupervisor  =  false; 
-		__mtxQS.unlock();
-		result = 1;
-	}
-	//return result; // tmp
-	return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+  if(!isWrite) {
+    __mtx.lock();
+    __buffer.pop_front();
+    __buffer.push_back(_pVecPut);
+    for(auto& it: __clients) {
+      if(it.second < __nMaxBuffers)
+        it.second = it.second + 1;
+    }
+    __mtx.unlock();
+    
+    __mtxQS.lock(); 
+    __qSupervisor.pop();  
+    if( __qSupervisor.size() > 0) {
+      __supervisor  =  __qSupervisor.front(); 
+      __isSupervisor  =  true;
+    }
+    else __isSupervisor  =  false; 
+    __mtxQS.unlock();
+    result = 1;
+  }
+  //return result; // tmp
+  return std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
 }
 
 int ISplitter::SplitterFlush() 
 {
-	bool isRun = true;
+  bool isRun = true;
   long ID = static_cast<long>(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
-	__mtxQS.lock(); 
-	__qSupervisor.push(ID);  
-	__supervisor	=  __qSupervisor.front(); 
-	__isSupervisor	=  true; 
-	__mtxQS.unlock();
-	while(isRun) {
-			if( __supervisor == ID) {
+  __mtxQS.lock(); 
+  __qSupervisor.push(ID);  
+  __supervisor  =  __qSupervisor.front(); 
+  __isSupervisor  =  true; 
+  __mtxQS.unlock();
+  while(isRun) {
+      if( __supervisor == ID) {
       __mtx.lock();
-			__buffer.clear();
-			for(auto& it: __clients)
-				it.second = -1;
-			__mtx.unlock();
+      __buffer.clear();
+      for(auto& it: __clients)
+        it.second = -1;
+      __mtx.unlock();
       isRun = false;
       }
   }
-		__mtxQS.lock(); 
-		__qSupervisor.pop();  
-		if( __qSupervisor.size() > 0) {
-			__supervisor	=  __qSupervisor.front(); 
-			__isSupervisor	=  true;
-		}
-		else __isSupervisor  =  false; 
-		__mtxQS.unlock();
+    __mtxQS.lock(); 
+    __qSupervisor.pop();  
+    if( __qSupervisor.size() > 0) {
+      __supervisor  =  __qSupervisor.front(); 
+      __isSupervisor  =  true;
+    }
+    else __isSupervisor  =  false; 
+    __mtxQS.unlock();
 
   return 0;
 }
 
 bool ISplitter::SplitterClientAdd(int* _pnClientID) 
 {
-	int ID = static_cast<int>(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
-	*_pnClientID = ID;
-	bool result = false;
+  int ID = static_cast<int>(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
+  *_pnClientID = ID;
+  bool result = false;
   __mtx.lock();
   if(__clients.find(ID) != __clients.end()) result = false;
   else {
-		if((int)__clients.size() < __nMaxClients) {
-    	__clients.insert(std::pair<int, int>(*_pnClientID, 0));
-			result = true;
-		}
-		else result = false;
-	}  
+    if((int)__clients.size() < __nMaxClients) {
+      __clients.insert(std::pair<int, int>(*_pnClientID, 0));
+      result = true;
+    }
+    else result = false;
+  }  
   __mtx.unlock();
-	return  result;
+  return  result;
   
 } 
 
@@ -190,16 +190,16 @@ __mtx.lock();
   size = __clients.size();
   if(_nIndex >= __nMaxClients || _nIndex >= size) isFound = false;
   else {
-		int n = 0;
-  	for(auto& it: __clients) {
-			if(n ==  _nIndex) {
-    		*_pnClientID = it.first;
-    		*_pnLatency = it.second;
-    		isFound = true;
-				break;
-			}
-    	n++;
-  	}
+    int n = 0;
+    for(auto& it: __clients) {
+      if(n ==  _nIndex) {
+        *_pnClientID = it.first;
+        *_pnLatency = it.second;
+        isFound = true;
+        break;
+      }
+      n++;
+    }
   }
   __mtx.unlock();
   return isFound;
@@ -209,86 +209,86 @@ int ISplitter::SplitterGet(int _nClientID, std::shared_ptr<std::vector<uint8_t>>
 {
   auto t1 = std::chrono::high_resolution_clock::now();
   
-	int err = 0;
-	int count = 0;
+  int err = 0;
+  int count = 0;
   std::map<int,int>::iterator it;
   bool isRun = true;
  
-	__mtxQC.lock(); 
-	__qClients.push_back(_nClientID);  
-	__client	=  __qClients.front(); 
-	__mtxQC.unlock();
+  __mtxQC.lock(); 
+  __qClients.push_back(_nClientID);  
+  __client  =  __qClients.front(); 
+  __mtxQC.unlock();
   
   auto t2 = std::chrono::high_resolution_clock::now();
-	while(isRun && 
-				std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() < _nTimeOutMsec) {
-  	if(__client == _nClientID && !__isSupervisor) {
-			
-			__mtx.lock();
-  		it = __clients.find(_nClientID);
-  		if(it == __clients.end())  {
-				err = 1;
-				isRun = false;
-			}
-  		else {
-				count = it->second;
-  			if(count <= 0) t2 = std::chrono::high_resolution_clock::now();
-				else {
-  				_pVecGet = __buffer.at(__buffer.size() - count);
-					it->second =  it->second - 1;
-					isRun = false;			
-				}
-			} 
-			__mtx.unlock();
-			
-			__mtxQC.lock(); 
-			__qClients.pop_front();
-			__client	=  __qClients.front(); 
-			if(isRun) __qClients.push_back(_nClientID);  
-			__mtxQC.unlock();
-		}
-		else t2 = std::chrono::high_resolution_clock::now();
+  while(isRun && 
+        std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() < _nTimeOutMsec) {
+    if(__client == _nClientID && !__isSupervisor) {
+      
+      __mtx.lock();
+      it = __clients.find(_nClientID);
+      if(it == __clients.end())  {
+        err = 1;
+        isRun = false;
+      }
+      else {
+        count = it->second;
+        if(count <= 0) t2 = std::chrono::high_resolution_clock::now();
+        else {
+          _pVecGet = __buffer.at(__buffer.size() - count);
+          it->second =  it->second - 1;
+          isRun = false;      
+        }
+      } 
+      __mtx.unlock();
+      
+      __mtxQC.lock(); 
+      __qClients.pop_front();
+      __client  =  __qClients.front(); 
+      if(isRun) __qClients.push_back(_nClientID);  
+      __mtxQC.unlock();
+    }
+    else t2 = std::chrono::high_resolution_clock::now();
   }
-	if(isRun) {
-		err = 3;
-		__mtxQC.lock(); 
-			for (auto i = __qClients.begin(); i != __qClients.end();) {
-  			if(*i == _nClientID) {
-    			i = __qClients.erase(i); 
-					break;
-				}
-  			else ++i;
-			}
-		__mtxQC.unlock();
-	}
+  if(isRun) {
+    err = 3;
+    __mtxQC.lock(); 
+      for (auto i = __qClients.begin(); i != __qClients.end();) {
+        if(*i == _nClientID) {
+          i = __qClients.erase(i); 
+          break;
+        }
+        else ++i;
+      }
+    __mtxQC.unlock();
+  }
   return err;
 }
 
 void ISplitter::SplitterClose()
 {
   std::cout << "Hello";
-	bool isRun = true;
+  bool isRun = true;
   long ID = static_cast<long>(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count());
-	__mtxQS.lock(); 
-	__qSupervisor.push(ID);  
-	__supervisor	=  __qSupervisor.front(); 
-	__isSupervisor	=  true; 
-	__mtxQS.unlock();
-	while(isRun) {
-			if( __supervisor == ID) {
+  __mtxQS.lock(); 
+  __qSupervisor.push(ID);  
+  __supervisor  =  __qSupervisor.front(); 
+  __isSupervisor  =  true; 
+  __mtxQS.unlock();
+  while(isRun) {
+      if( __supervisor == ID) {
       __mtx.lock();
-			__buffer.clear();
+      __buffer.clear();
       __clients.clear();
-			__mtx.unlock();
+      __mtx.unlock();
       isRun = false;
       }
   }
-		__mtxQS.lock(); 
-		__qSupervisor.pop();  
-		if( __qSupervisor.size() > 0) {
-			__supervisor	=  __qSupervisor.front(); 
-			__isSupervisor	=  true;
-		}
-		else __isSupervisor  =  false; 
-		__mtxQS.unlock();
+    __mtxQS.lock(); 
+    __qSupervisor.pop();  
+    if( __qSupervisor.size() > 0) {
+      __supervisor  =  __qSupervisor.front(); 
+      __isSupervisor  =  true;
+    }
+    else __isSupervisor  =  false; 
+    __mtxQS.unlock();
 }
